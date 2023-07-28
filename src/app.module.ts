0b0960c6813c21ffdd801881b10564fe583b95a0
anclaev <iahugo@yandex.ru>
2023-07-28 23:13:28 +0300

    Заготовки сущностей.

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,15 @@ import { AuthModule } from './auth/auth.module'
 import { CommonModule } from '@common/common.module'
 import { PrismaModule } from './prisma/prisma.module'
 import { UserModule } from './user/user.module'
+import { ProductModule } from './product/product.module';
+import { ReviewModule } from './review/review.module';
+import { CategoryModule } from './category/category.module';
+import { OrderModule } from './order/order.module';
+import { PaginationModule } from './pagination/pagination.module';
+import { MetricsModule } from './metrics/metrics.module';
 
 @Module({
-  imports: [ConfigModule, CommonModule, PrismaModule, UserModule, AuthModule],
+  imports: [ConfigModule, CommonModule, PrismaModule, UserModule, AuthModule, ProductModule, ReviewModule, CategoryModule, OrderModule, PaginationModule, MetricsModule],
   controllers: [AppController],
   providers: [AppService],
 })
